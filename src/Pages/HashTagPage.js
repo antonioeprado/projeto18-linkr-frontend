82f3a2b2191b36499fabfdf88dd97e1d0f0afb64
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-01-04 21:13:57 -0300

    refact hashtagpage

--- src/Pages/HashTagPage.js
+++ src/Pages/HashTagPage.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import Header from "../Components/Constants/Header";
 import Post from "../Components/TimelinePage/Post";
 import Trending from "../Components/TimelinePage/Trending";
-import { useParams } from "react-router-dom";
 
 export default function HashTagsPage() {
 	const { hashtag } = useParams();

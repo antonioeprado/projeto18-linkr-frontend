9a34b02dceb2adecc4521150a0f91fb986d83f66
isabellexvr <isabellexavier59@gmail.com>
2023-01-08 14:27:40 -0300

    fixed: reload page functions

--- src/Components/TimelinePage/Post.js
+++ src/Components/TimelinePage/Post.js
@@ -9,6 +9,11 @@ import { postLikeFunction, dislikeFunction } from "../Services/LikeFunctions";
 import { useContext } from "react";
 import { AuthContext } from "../Context/authContext";
 import jwtDecode from "jwt-decode";
+import {
+  LoadingMessage,
+  NoPostsMessage,
+  ErrorMessage,
+} from "./SmallComponents/AlternativeMessages";
 
 export default function Post({ loading, setLoading }) {
   const navigate = useNavigate();
@@ -26,22 +31,21 @@ export default function Post({ loading, setLoading }) {
   };
 
   //https://linkr-api-9ik9.onrender.com/
-
+/* 
   useEffect(() => {
     axios
       .get("http://localhost:4000/all-posts")
       .then((a) => {
         setLoading(false);
         setPosts(a.data);
-        console.log(a.data)
-
+        console.log(a.data);
       })
       .catch((e) => {
         setLoading(false);
         setError(true);
         console.log(e);
       });
-  }, [loading, setLoading, setLiked, liked]);
+  }, []); */
 
   //se e.likedBy.length > 0, procura o id do user com: e.likedBy.find((l) => l.userId === userId) pra poder mostrar vermelho
 
@@ -118,33 +122,9 @@ export default function Post({ loading, setLoading }) {
           ))}
         </>
       )}
-      {!error && loading && (
-        <>
-          <LoadingMessage>
-            <div>
-              <h1>Loading posts...</h1>
-            </div>
-          </LoadingMessage>
-        </>
-      )}
-      {!error && !loading && posts.length < 1 && (
-        <NoPostsMessage>
-          <h1>There are no posts yet.</h1>
-          <button onClick={() => Location.reload()}>Reload</button>
-        </NoPostsMessage>
-      )}
-      {error && (
-        <ErrorMessage>
-          <div>
-            <h1>ERROR</h1>
-            <h2>
-              An error occured while trying to fetch the posts, please refresh
-              the page clicking the button down below.
-            </h2>
-            <button onClick={() => Location.reload()}>Reload</button>
-          </div>
-        </ErrorMessage>
-      )}
+      {!error && loading && <LoadingMessage />}
+      {!error && !loading && posts.length < 1 && <NoPostsMessage />}
+      {error && <ErrorMessage />}
     </>
   );
 }
@@ -159,79 +139,6 @@ const PostStyle = styled.div`
     width: 611px;
     height: 276px;
     border-radius: 16px;
-  }
-`;
-
-const LoadingMessage = styled.div`
-  margin-top: 25px;
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  > div {
-    height: 50px;
-    width: 60%;
-    border-radius: 15px;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    > h1 {
-      font-family: "Lato";
-      font-weight: 600;
-      font-size: 18px;
-    }
-  }
-`;
-
-const ErrorMessage = styled.div`
-  margin-top: 25px;
-  font-family: "Lato";
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  > div {
-    height: 125px;
-    background-color: white;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    > h1 {
-      color: red;
-      font-weight: 800;
-      font-size: 20px;
-      margin-bottom: 5px;
-    }
-    > h2 {
-      font-size: 14px;
-      margin-bottom: 10px;
-      width: 90%;
-      text-align: justify;
-    }
-    > button {
-      background-color: #1877f2;
-      border-radius: 5px;
-      width: 112px;
-      height: 22px;
-      border: none;
-      color: #ffffff;
-      font-family: "Lato";
-      font-weight: 700;
-      font-size: 13px;
-    }
-  }
-`;
-
-const NoPostsMessage = styled.div`
-  margin-top: 25px;
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  > h1 {
-    font-family: "Lato";
-    font-weight: 600;
-    font-size: 18px;
-    color: #efefef;
   }
 `;
 
